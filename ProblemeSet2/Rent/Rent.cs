74e22cb7be5c6b02d08fc0fe30fd51baea4dfe99
iliesbogdan <sbbogdan@yahoo.com>
2015-12-10 00:06:58 +0200

    Trying to find a method to get a value from
    from the first method

--- ProblemeSet2/Rent/Rent.cs
+++ ProblemeSet2/Rent/Rent.cs
@@ -9,30 +9,34 @@ namespace Rent
         [TestMethod]
         public void TestMethod1()
         {
-            float showHowMuchRent = calculateRent(100,5);
+            float showHowMuchRent = calculateRentPenalties(100,5);
             Assert.AreEqual(10, showHowMuchRent);
         }
-        float calculateRent (int rent, int days)
+        float calculateRentPenalties (int rent, int days)
         {
+            // int theRentPenalties
             if (days <= 10)
 
-                return (rent * 2 / 100 * days);
+           return (rent * 2 / 100 * days); //theRentPenalties = (rent * 2 / 100 * days);
 
-             if (days <= 30)
+            if (days <= 30)
 
-                return (rent * 5 / 100 * days);
+           return (rent * 5 / 100 * days);
 
              if (days <= 40)
 
-                return (rent * 10 / 100 * days);
+           return (rent * 10 / 100 * days);
 
-                return (rent * 100/100*days);
-            
+           return (rent * 100 / 100 * days);
+          // trying to change the return with an variable, and save the result in it
+          //then use it      
         }
+        float theFinalRentPrice(float finalRent)
+        {
+            // trying to find a method to get the value from the function calculateRentPenalties
+            // and then to find the final price for the final rent, and then test it.
 
-
-
-
+        }
 
 
     }
