c10b4ced93ae3ef398311e9a07ea59d0e2df47dd
iliesbogdan <sbbogdan@yahoo.com>
2015-12-09 18:29:48 +0200

    Add  new function to get results

--- ProblemeSet2/FizzBuzz/FizzBuzz.cs
+++ ProblemeSet2/FizzBuzz/FizzBuzz.cs
@@ -9,8 +9,27 @@ namespace FizzBuzz
         [TestMethod]
         public void TestBuzzFizz() {
             
-        }     
-	    }
+        }
+        string getFizzBuzz(int a)
+        {
+
+            string f = "FIZZ";
+            string b = "BUZZ";
+            string fb = "FIZZBUZZ";
+
+            if (a % 5 == 0 && a % 3 == 0)
+                return fb;
+
+            else
+                if (a % 5 == 0)
+                return b;
+
+            else if (a % 3 == 0)
+                return f;
+            else return "none";
+
+        }
+    }
         }
     }
 
