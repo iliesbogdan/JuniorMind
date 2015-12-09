0b64ac24cca1661ed5b2dacccdcf88e199a4e8d7
iliesbogdan <sbbogdan@yahoo.com>
2015-12-09 23:44:41 +0200

    Added test to get rent penalties and method to determine the penalties

--- ProblemeSet2/Rent/Rent.cs
+++ ProblemeSet2/Rent/Rent.cs
@@ -4,12 +4,37 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Rent
 {
     [TestClass]
-    public class UnitTest1
+    public class Rent
     {
         [TestMethod]
         public void TestMethod1()
         {
+            float showHowMuchRent = calculateRent(100,5);
+            Assert.AreEqual(10, showHowMuchRent);
         }
+        float calculateRent (int rent, int days)
+        {
+            if (days <= 10)
+
+                return (rent * 2 / 100 * days);
+
+             if (days <= 30)
+
+                return (rent * 5 / 100 * days);
+
+             if (days <= 40)
+
+                return (rent * 10 / 100 * days);
+
+                return (rent * 100/100*days);
+            
+        }
+
+
+
+
+
+
     }
 
 
