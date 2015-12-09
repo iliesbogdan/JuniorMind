ded6986ecca5a8f22806e5fb7fce40744242f0cd
iliesbogdan <sbbogdan@yahoo.com>
2015-12-09 18:26:00 +0200

    Add  new project test FIzzBuzz

---
+++ ProblemeSet2/FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FizzBuzz
+{
+    [TestClass]
+    public class FizzBuzz
+    {
+        [TestMethod]
+        public void TestBuzzFizz() {
+            
+        }     
+	    }
+        }
+    }
+
